4b9718f3bcd99535f99b988090fa44e9bbf0717d
Tusar <tusarahmed141@gmail.com>
2024-09-17 16:38:47 +0600

    Last update

---
+++ Day_02[Data-Type]/Datatype.md
@@ -0,0 +1,22 @@
+# Object Type / Data Type
+
+# Numbers: 1234, 3.15, 3+4j, 0b111, Decimal();
+
+
+
+# String : 'Tusar', "Teacher's", b'a\x01c', u'sp\xc4m;
+str1 = b'a\x01c';
+str2 = u'sp\xc4m';
+print(str1)
+print(str2)
+
+# List : [1, [2, 'three'], 4.5] list(range(10))
+
+# Tuple: (1, 'span')
+# Dictionary
+# Set 
+# File
+# Boolean
+# None : None
+# Function, module, classes 
+# Advanced: Decorators, Generators, Iterators, MetaProgramming
--- Day_02[Data-Type]/app.py
+++ Day_02[Data-Type]/app.py
@@ -1,22 +1,11 @@
-# Object Type / Data Type
+# Formatting a message
+name = "Alice"
+message = f"Hello, {name}!"  # message is "Hello, Alice!"
+print(message)
+# Extracting a substring
+text = "Python programming"
+first_word = text.split()  # first_word is "Python"
+print(first_word)
 
-# Numbers: 1234, 3.15, 3+4j, 0b111, Decimal();
-
-
-
-# String : 'Tusar', "Teacher's", b'a\x01c', u'sp\xc4m;
-str1 = b'a\x01c';
-str2 = u'sp\xc4m';
-print(str1)
-print(str2)
-
-# List : [1, [2, 'three'], 4.5] list(range(10))
-
-# Tuple: (1, 'span')
-# Dictionary
-# Set 
-# File
-# Boolean
-# None : None
-# Function, module, classes 
-# Advanced: Decorators, Generators, Iterators, MetaProgramming
+n = True;
+print(not n);
