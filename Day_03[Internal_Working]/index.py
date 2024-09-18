686ec3d760196f926aad5763030f8c94e64060ed
Tusar <tusarahmed141@gmail.com>
2024-09-18 19:33:31 +0600

    Today lean some indepth sence

--- Day_03[Internal_Working]/index.py
+++ Day_03[Internal_Working]/index.py
@@ -3,10 +3,22 @@
 # myListOne[1] = "Rakib";
 # print(myListTwo);
 
-x = [1,2,3];
-y = x[:];
-print(x);
-x[0] = "Rakib"
-print(x)
-print(y);
+# x = [1,2,3];
+# y = x[:];
+# print(x);
+# x[0] = "Rakib"
+# print(x)
+# print(y);
+
+import copy
+
+# x = [10,20,30, ["rakib", "Tusar"]];
+# y = x.copy();
+# x[3:] = "1";
+# print(x);
+# print(y);
+
+x = [1,2,3]; 
+y = [1,2,3];
+print(x == y);
 
