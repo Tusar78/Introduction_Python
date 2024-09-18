ceb44572217f505d5d2275853da37a0a243df88b
Tusar <tusarahmed141@gmail.com>
2024-09-18 19:22:22 +0600

    Learned python copy trick

---
+++ Day_03[Internal_Working]/app.js
@@ -0,0 +1,8 @@
+let a = 10;
+a = a + 5;
+console.log(a);
+
+let myArr = [1,2,3];
+let myArr2 = myArr;
+myArr[2] = "Rakib";
+console.log(myArr2);
---
+++ Day_03[Internal_Working]/index.py
@@ -0,0 +1,12 @@
+# myListOne = [1,2,3];
+# myListTwo = myListOne;
+# myListOne[1] = "Rakib";
+# print(myListTwo);
+
+x = [1,2,3];
+y = x[:];
+print(x);
+x[0] = "Rakib"
+print(x)
+print(y);
+
