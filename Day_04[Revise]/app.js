37e097db4097be3338e95e4ad440ed673b4b6028
Tusar <tusarahmed141@gmail.com>
2024-10-02 16:26:34 +0600

    Here two practice done over  integer number

---
+++ Day_04[Revise]/app.js
@@ -0,0 +1,3 @@
+const age = +prompt("Enter your age: ");
+toHundred = 100 - age;
+console.log(`You will turn 100 in ${toHundred} years.`);
---
+++ Day_04[Revise]/index.py
@@ -0,0 +1,16 @@
+# Practice 1: Create a program that asks the user for their age and then calculates how many years it will take for them to turn 100.
+
+# userAge = int(input("Enter your age: "))
+# toHundred = 100 - userAge
+# print(f"You will turn 100 in {toHundred} years")
+
+
+# Practice 2: Write a program that calculates the total price of items in a shopping cart, where the prices are integers.
+
+item1 = 75;
+item2 = 78;
+
+total = item1 + item2;
+
+# printing the total item 
+print(f"The total item price is {total}$")
